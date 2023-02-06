259d4e068889528eb11ac7fd41097cb06093dcbd
reaganongaya <reagan.ongaya@student.moringaschool.com>
2023-02-06 22:29:01 +0300

    Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Jimmy")
+  puts "Hello, #{name}!"
+end 
+
+greet   
+
+greet "Naureen"
+
+def greet_with_default(name ="programmer" )
+    puts "Hello, #{name}!"
+end 
+
+greet_with_default
+
+greet_with_default "Naureen"
+
+def add (num1,num2)
+    num1 + num2
+end
+
+add(2, 3)
+
+def halve()
+    
+end
+
